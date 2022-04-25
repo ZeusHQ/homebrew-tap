96a6b218df4a1600e225c5129a9b40338a540c89
Eric Campbell <ericcampbell59@gmail.com>
2022-04-25 13:53:36 -0500

    fix repo name

--- Formula/zproxy.rb
+++ Formula/zproxy.rb
@@ -1,7 +1,7 @@
 class Zproxy < Formula
     desc "A a zero-configuration ReverseProxy for Turborepos that automagically proxies all http and websocket requests from local domains to their respective localhost:port url."
-    homepage "https://github.com/zeushq/zproxy"
-    url "https://github.com/zeushq/zproxy/archive/v1.0.2.tar.gz"
+    homepage "https://github.com/zeushq/zproxy-go"
+    url "https://github.com/ZeusHQ/zproxy-go/archive/refs/tags/v1.0.2.tar.gz"
     sha256 "a2f77edfb44d3de5a683ee32d1550b404cbf62ea6d2d46ea6f86a0ab9317d39e"
   
     depends_on "go"
@@ -11,8 +11,8 @@ class Zproxy < Formula
       ENV["GO111MODULE"] = "on"
       ENV["GOFLAGS"] = "-mod=vendor"
       ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
-      (buildpath/"src/github.com/zeushq/zproxy").install buildpath.children
-      cd "src/github.com/zeushq/zproxy" do
+      (buildpath/"src/github.com/zeushq/zproxy-go").install buildpath.children
+      cd "src/github.com/zeushq/zproxy-go" do
         system "go", "build", "-o", bin/"zproxy", "."
       end
     end
